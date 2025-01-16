7ac9b070e120f02a91b31382fe7d53d91d4096f2
Vannelo <work@vannelo.com>
2025-01-16 09:47:09 -0600

    Fix params

--- app/c/[id]/page.tsx
+++ app/c/[id]/page.tsx
@@ -1,7 +1,16 @@
+import { notFound } from "next/navigation";
 import LandingPageClient from "./pageClient";
 
-export default async function Landing({ params }: { params: { id: string } }) {
-  const codigo = params.id;
+export default async function Landing({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const codigo = (await params).id;
+
+  if (!codigo) {
+    notFound();
+  }
 
   return <LandingPageClient codigo={codigo} />;
 }
